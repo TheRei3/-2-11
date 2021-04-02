cd972b5ae09ffd29cef9ce7bc51a45ef65ee3043
admin <admin@DESKTOP-8L2IG92>
2021-04-03 06:56:57 +0900

    ex2no11

---
+++ ex2no11/src/ex2no11/ex2no11.java
@@ -0,0 +1,11 @@
+package ex2no11;
+
+public class ex2no11 {  //73p
+     public static void main(String[] args) {
+		float f = 9.1234567f;
+		int i = Float.floatToIntBits(f);
+		
+		System.out.printf("%f%n",f);
+		System.out.printf("%X%n",i);
+	}
+}
